e2f2f7c8e9b4b85dff9035314e162dc6695a8e9f
JP Zhang <zjplab@gmail.com>
2020-02-02 11:46:35 -0500

    Update postorder.cpp

--- tree/traversal/postorder.cpp
+++ tree/traversal/postorder.cpp
@@ -1,1 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        postorder(root, res);
+        return res;
+    }
+    
+private:
+    void postorder(TreeNode *root, vector<int> &res){
+        if(!root) return;
+        postorder(root->left, res);
+        postorder(root->right, res);
+        res.push_back(root->val);
+    }
+};
+
+
+//Non-recursive version 
+// Use a stack to emulate the function call stack 
+
 
