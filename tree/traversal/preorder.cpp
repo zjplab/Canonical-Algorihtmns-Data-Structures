443d22541fc7e92eb7f2fc7e36866c89e0becff2
JP Zhang <zjplab@gmail.com>
2021-09-20 10:13:58 +0800

    Update preorder.cpp

--- tree/traversal/preorder.cpp
+++ tree/traversal/preorder.cpp
@@ -25,6 +25,46 @@ private:
 };
 
 
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root==nullptr) return {};
+        vector<int> res;
+        TreeNode* p(root);
+        stack<TreeNode*> s;
+        while(p!=nullptr || s.size()){
+            if(p!=nullptr){
+                res.emplace_back(p->val);
+                if(p->right!=nullptr ) s.push(p->right);
+                p=p->left;
+            }
+            else{
+                p=s.top(); s.pop();
+                res.emplace_back(p->val);
+                if(p->right!=nullptr ) s.push(p->right);
+                p=p->left;
+            }
+        }
+        return res;
+    }
+};
+
+//Version 2
+
+
 //Non-recursive version 
 // Use a stack to emulate recursion
  /* Definition for a binary tree node.
